14b675ac78472441b1f96cc158a3f5c9e53ba9b6
Neïl Aloulou <98472708+JoyfulCanis@users.noreply.github.com>
2023-04-17 19:27:49 +0200

    directory 'controllers' and code split

--- routes/stuff.js
+++ routes/stuff.js
@@ -1,46 +1,20 @@
 const express = require("express");
 const router = express.Router();
-
-const Thing = require("../models/Thing");
+const stuffCtrl = require("../controllers/stuff");
 
 //------------POST------Nouvel--objet------------------
-router.post("/", (req, res, next) => {
-  delete req.body._id;
-  const thing = new Thing({
-    ...req.body,
-  });
-  thing
-    .save()
-    .then(() => res.status(201).json({ message: "Objet enregistré !" }))
-    .catch((error) => res.status(400).json({ error }));
-});
-//-------------GET-------Page--Produit----------------
-router.get("/:id", (req, res, next) => {
-  Thing.findOne({ _id: req.params.id })
-    .then((thing) => res.status(200).json(thing))
-    .catch((error) => res.status(404).json({ error }));
-});
+router.post("/", stuffCtrl.createThing);
 
-//--------UPDATE---------
-router.put("/:id", (req, res, next) => {
-  Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-    .then(() => res.status(200).json({ message: "Objet modifié !" }))
-    .catch((error) => res.status(400).json({ error }));
-});
+//--------------GET------PAGE-PRODUIT---------------
+router.get("/:id", stuffCtrl.getOneThing);
+
+//--------UPDATE---------EDITION------------
+router.put("/:id", stuffCtrl.modifyThing);
 
 //---------DELETE---------
-router.delete("/:id", (req, res, next) => {
-  Thing.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: "Objet supprimé !" }))
-    .catch((error) => res.status(400).json({ error }));
-});
+router.delete("/:id", stuffCtrl.deleteThing);
 
-//-------------GET-------Page--d'accueil----------------
-router.get("/", (req, res, next) => {
-  //On peut y ajouter des arguments pour chercher des objets spécifiques, par date par ex ou par type dans le find(ICI)
-  Thing.find()
-    .then((things) => res.status(200).json(things))
-    .catch((error) => res.status(400).json({ error }));
-});
+//----------GET---------PAGE-D'ACCUEIL----------------
+router.get("/", stuffCtrl.getAllThings);
 
 module.exports = router;
